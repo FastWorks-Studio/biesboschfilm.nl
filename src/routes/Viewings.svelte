29771df0d195b12c0d1928b392d800219f61f55c
Coen Koopmans <coen.koopmans@kpn.com>
2024-10-18 15:12:50 +0200

    fix html for viewing

--- src/routes/Viewings.svelte
+++ src/routes/Viewings.svelte
@@ -146,7 +146,6 @@
     </div>
     <div class="buy">
       <Button noMargin primary click="https://www.lux-nijmegen.nl/programma/film-gesprek-de-biesbosch-natuur-in-beweging/">Bestel kaarten</Button>
-      <div class="not-available">Nog niet beschikbaar</div>
     </div>
   </li>
 </ul>
